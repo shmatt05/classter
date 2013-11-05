966bd6539dda4c3d72b56d09a74656612318b26d
Omri <omri.sl@gmail.com>
2013-11-05 17:01:06 +0200

    omri

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello every1 its omri')
+        self.response.write('Hello every1 its Omri')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
