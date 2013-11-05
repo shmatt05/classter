b000b49aaf5be872c8c775e87d3ea442c47c6df9
David <davidfra@mail.tau.ac.il>
2013-11-05 16:46:09 +0200

    Jump String

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello everyoneeee!!!!!')
+        self.response.write('Hello every1\nJump!!!!!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
