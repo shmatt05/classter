a53cde688ddfda172afc05ab6d74dff071c00b7c
David <davidfra@mail.tau.ac.il>
2013-11-05 14:34:38 +0200

    ADDED PUSHPUSH

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello everyone!! FUCKING!')
+        self.response.write('Hello everyone!! PUSHPUSH!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
