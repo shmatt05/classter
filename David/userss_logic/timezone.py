e333b6bf8a51a74fa9e8e79b9f8072f8058623d9
Roy <ro.klinger@gmail.com>
2013-11-15 10:25:15 +0200

    Added Time class

--- David/userss_logic/timezone.py
+++ David/userss_logic/timezone.py
@@ -19,3 +19,6 @@ class Time():
     """ return the date of now + delta """
     def get_date_with_delta(self, delta):
         return datetime.now(self.tz_time) + timedelta(delta)
+
+    def __add__(self, day):
+        return self.get_date_with_delta(day)
