8454fff5f70d1d4c84b1dfdb3d33486ffa39e804
Matan <shmatt@gmail.com>
2014-01-12 13:23:41 +0200

    new about popup, small changes

--- templates/lib/js/user-grid-jq.js
+++ templates/lib/js/user-grid-jq.js
@@ -6,104 +6,118 @@ var changeWeekVar = new Date().getTime();
 
 $(document).ready(function() {
 
-    $('#welcomediv').find('img').each(function(){
-        var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
-        $(this).addClass(imgClass);
-    })
-    changeWeek(new Date().getTime()); //initialize first date screen
+    $('#about').on('click',  function(e) {
+        $.magnificPopup.open({
+            type:'inline',
+            items: {
+                src: '#aboutpopup'
 
-    // Calendar Date Limitations (Back / Forward)
-    var past = new Date();
-    past.addDays(-7);
-    var future = new Date();
-    future.addDays(14);
+            },
 
-    $('#calendar').weekCalendar({
-        data: function (start, end, callback) {
-            callback(classesTableArr);
-            for (var i=0; i<classesTableArr.length; i++) {
-                $('#'+classesTableArr[i].id + ' .lblInstructor').text(classesTableArr[i].instructor);
-                $('#'+classesTableArr[i].id + ' .lblOpenSlots').text(classesTableArr[i].openSlots+ ' מקומות');
-               // if ($('#'+classesTableArr[i].id).css('background-color') != 'rgb(170, 170, 170)') {
-                    $('#'+classesTableArr[i].id).css('background-color',classesTableArr[i].color);
-              //  }
-                if (classesTableArr[i].isReg) {
-                    $('#'+classesTableArr[i].id + ' .indicator').css('background-color','#33CC33');
-                }
-                else {
-                    $('#'+classesTableArr[i].id + ' .indicator').css('background-color','#990000');
-                }
+            closeOnContentClick: false
 
-            }
-        },
-        buttonText: {
-            today:'היום',
-            lastWeek:'קודם',
-            nextWeek:'הבא'
+        });
 
-        },
-        use24Hour:true,
-        changedate: function($calendar, date) {
-            changeWeek(date.getTime());
-
-        },
-        timeslotsPerHour: 4,
-        defaultEventLength:4,
-        timeSeparator: ' - ',
-        buttons:true,
-        minDate:past,
-        maxDate:future,
-        longDays:['ראשון','שני','שלישי','רביעי','חמישי','שישי','שבת'],
-        longMonths:['ינואר','פברואר','מרץ','אפריל','מאי','יוני','יולי','אוגוסט','ספטמבר','אוקטובר','נובמבר','דצמבר'],
-        shortMonths:['ינואר','פברואר','מרץ','אפריל','מאי','יוני','יולי','אוגוסט','ספטמבר','אוקטובר','נובמבר','דצמבר'],
-        startOnFirstDayOfWeek:true,
-        businessHours:{start:6, end:23, limitDisplay: true},
-        dateFormat:"d/m/Y",
-        allowCalEventOverlap: true,
-        overlapEventsSeparate: true,
-        totalEventsWidthPercentInOneColumn : 100,
-        readonly:true,
-
-        height: function($calendar) {
-            return $(window).height() - $('h1').outerHeight(true);
-        },
-        eventRender: function(calEvent, $event) {
-            if (calEvent.end.getTime() < new Date().getTime()) {
-                //$event.css('backgroundColor', '#aaa');
-                $event.css('opacity', '0.3');
-
-            }
-        },
-        eventNew: function(calEvent, $event) {
-            displayMessage('<strong>Added event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-            alert('You\'ve added a new event. You would capture this event, add the logic for creating a new event with your own fields, data and whatever backend persistence you require.');
-        },
-        eventDrop: function(calEvent, $event) {
-            displayMessage('<strong>Moved Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-        },
-        eventResize: function(calEvent, $event) {
-            displayMessage('<strong>Resized Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-        },
-        eventClick: function(calEvent, $event) {
-            console.log(calEvent.id);
-            openPopup(calEvent.id, calEvent.start);
-        },
-        eventMouseover: function(calEvent, $event) {
-            displayMessage('<strong>Mouseover Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-        },
-        eventMouseout: function(calEvent, $event) {
-            displayMessage('<strong>Mouseout Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
-        },
-        noEvents: function() {
-            displayMessage('There are no events for this week');
-        }
     });
 
-    function displayMessage(message) {
-        $('#message').html(message).fadeIn();
-    }
+$('#welcomediv').find('img').each(function(){
+    var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
+    $(this).addClass(imgClass);
+})
+changeWeek(new Date().getTime()); //initialize first date screen
 
-    //$('<div id="message" class="ui-corner-all"></div>').prependTo($('body'));
+// Calendar Date Limitations (Back / Forward)
+var past = new Date();
+past.addDays(-7);
+var future = new Date();
+future.addDays(14);
+
+$('#calendar').weekCalendar({
+    data: function (start, end, callback) {
+        callback(classesTableArr);
+        for (var i=0; i<classesTableArr.length; i++) {
+            $('#'+classesTableArr[i].id + ' .lblInstructor').text(classesTableArr[i].instructor);
+            $('#'+classesTableArr[i].id + ' .lblOpenSlots').text(classesTableArr[i].openSlots+ ' מקומות');
+            // if ($('#'+classesTableArr[i].id).css('background-color') != 'rgb(170, 170, 170)') {
+            $('#'+classesTableArr[i].id).css('background-color',classesTableArr[i].color);
+            //  }
+            if (classesTableArr[i].isReg) {
+                $('#'+classesTableArr[i].id + ' .indicator').css('background-color','#33CC33');
+            }
+            else {
+                $('#'+classesTableArr[i].id + ' .indicator').css('background-color','#990000');
+            }
+
+        }
+    },
+    buttonText: {
+        today:'היום',
+        lastWeek:'קודם',
+        nextWeek:'הבא'
+
+    },
+    use24Hour:true,
+    changedate: function($calendar, date) {
+        changeWeek(date.getTime());
+
+    },
+    timeslotsPerHour: 4,
+    defaultEventLength:4,
+    timeSeparator: ' - ',
+    buttons:true,
+    minDate:past,
+    maxDate:future,
+    longDays:['ראשון','שני','שלישי','רביעי','חמישי','שישי','שבת'],
+    longMonths:['ינואר','פברואר','מרץ','אפריל','מאי','יוני','יולי','אוגוסט','ספטמבר','אוקטובר','נובמבר','דצמבר'],
+    shortMonths:['ינואר','פברואר','מרץ','אפריל','מאי','יוני','יולי','אוגוסט','ספטמבר','אוקטובר','נובמבר','דצמבר'],
+    startOnFirstDayOfWeek:true,
+    businessHours:{start:6, end:23, limitDisplay: true},
+    dateFormat:"d/m/Y",
+    allowCalEventOverlap: true,
+    overlapEventsSeparate: true,
+    totalEventsWidthPercentInOneColumn : 100,
+    readonly:true,
+
+    height: function($calendar) {
+        return $(window).height() - $('h1').outerHeight(true);
+    },
+    eventRender: function(calEvent, $event) {
+        if (calEvent.end.getTime() < new Date().getTime()) {
+            //$event.css('backgroundColor', '#aaa');
+            $event.css('opacity', '0.3');
+
+        }
+    },
+    eventNew: function(calEvent, $event) {
+        displayMessage('<strong>Added event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+        alert('You\'ve added a new event. You would capture this event, add the logic for creating a new event with your own fields, data and whatever backend persistence you require.');
+    },
+    eventDrop: function(calEvent, $event) {
+        displayMessage('<strong>Moved Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+    },
+    eventResize: function(calEvent, $event) {
+        displayMessage('<strong>Resized Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+    },
+    eventClick: function(calEvent, $event) {
+        console.log(calEvent.id);
+        openPopup(calEvent.id, calEvent.start);
+    },
+    eventMouseover: function(calEvent, $event) {
+        displayMessage('<strong>Mouseover Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+    },
+    eventMouseout: function(calEvent, $event) {
+        displayMessage('<strong>Mouseout Event</strong><br/>Start: ' + calEvent.start + '<br/>End: ' + calEvent.end);
+    },
+    noEvents: function() {
+        displayMessage('There are no events for this week');
+    }
+});
+
+function displayMessage(message) {
+    $('#message').html(message).fadeIn();
+}
+
+//$('<div id="message" class="ui-corner-all"></div>').prependTo($('body'));
 
 
 
